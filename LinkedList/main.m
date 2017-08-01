e1847d941b033a1995390eeb040bcf401986a15b
曹臣 <caochen@caochendeiMac.local>
2017-08-01 08:48:34 +0800

    Initial Commit

---
+++ LinkedList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LinkedList
+//
+//  Created by 曹臣 on 2017/8/1.
+//  Copyright © 2017年 cc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
